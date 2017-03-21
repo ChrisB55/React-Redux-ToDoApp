bacc9054c83588e5ee0e4f2bdf7d35900d3f7ea1
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-03-21 15:35:43 -0400

    Add components to make listed items.

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,9 +1,32 @@
 const React = require('react');
+var TodoList = require('TodoList');
+
 var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'code component'
+        }, {
+          id: 2,
+          text: 'style component'
+        }, {
+          id: 3,
+          text: 'update component'
+        }, {
+          id: 4,
+          text: 'unit test component'
+        }
+      ]
+    };
+  },
   render: function () {
+    var {todos} = this.state;
+
     return (
       <div>
-        Root component
+        <TodoList todos={todos}/>
       </div>
     )
   }
